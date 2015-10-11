217d89a6f837fe4459429c0c301ecb840eec380b
masterUNG <phrombutr@gmail.com>
2015-10-11 09:13:08 +0700

    Start Day2

---
+++ app/src/main/java/appewtc/masterung/traffic/MainActivity.java
@@ -0,0 +1,32 @@
+package appewtc.masterung.traffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }
+
+}   // Main Class นี่คือ คลาสหลัก
